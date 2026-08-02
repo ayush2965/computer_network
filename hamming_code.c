848c15f90e5d4b04823312f7bd415528e1b7fb65
Ayush Deshmukh <ayde2420@gmail.com>
2026-08-02 23:57:12 +0530

    Update hamming_code.c

--- hamming_code.c
+++ hamming_code.c
@@ -22,12 +22,12 @@ void reverse(int input[20],int n,  int reverse[10]){
         reverse[j]=input[i];
         j++;
     }
-    display(reverse,n);
 }
 
-void hamming_code(int m[20],int m_size){
+void hamming_code(int in[20],int m_size){
     int r=0;
-    
+    int m[20];
+    reverse(in,m_size,m);
     while(pow(2,r)<(m_size+r+1)){
         r=r+1;
     }//find value of r 
@@ -78,14 +78,18 @@ void hamming_code(int m[20],int m_size){
             code_word[parity_position-1]=1;//if odd no of ones set parity bti one
         }
     }
+    int finalcw[20];
+    reverse(code_word,m_size+r,finalcw);
     printf("Code Word is: \n");
-    display(code_word,(m_size+r));
+    display(finalcw,(m_size+r));
 
         
 }
 
-void error_detection(int cw[20],int n){
+void error_detection(int in[20],int n){
     //calculate number of parity bits and their positions again
+    int cw[20];
+    reverse(in,n,cw);
     int r=1;
     int i=1;
     int r_positions[20];
@@ -95,17 +99,9 @@ void error_detection(int cw[20],int n){
         r_positions[r]=i;
         r++;
     }
-    printf("%d\n",r);
-    display(cw,n);
     printf("\n");
     printf("Redundant bit positions: ");
     display(r_positions,r);
-
-    // int reversedcw[n];
-    // int j=0;
-    // for(int i=0;i<n;i++){
-
-    // }
 
     int bits_at_parity[20];
     
@@ -138,31 +134,28 @@ void error_detection(int cw[20],int n){
 
     for(int i=0;i<r;i++){
         if(bits_at_codeword[i]!=bits_at_parity[i]){
-            printf("Change at bit position: %d ", r_positions[i]);
+            printf("Change at bit position: %d\n", r_positions[i]);
         }
     } 
-    int error_position[20];
-    int decrement=r-1;
+    int ans=0;
     for(int i=0;i<r;i++){
-        if(decrement==-1){
-            break;
-        }
-        error_position[decrement]=bits_at_parity[i];
-        decrement--;
+        ans+=(bits_at_parity[i]*(pow(2,i)));
     }
-    
-
-
+    printf("Error detected at bit position: %d (Decimal Value of Reversed Syndrome)\n",ans);
 }
 
 int main(){
-    // int data_bits_size;
-    // printf("Enter the size of the data bits (m): ");
-    // scanf("%d",&data_bits_size);
-    // int m[20];
-    // accept(m,data_bits_size);
-    // display(m,data_bits_size);
-    // hamming_code(m,data_bits_size);
+    printf("Sender's side\n");
+    int data_bits_size;
+    printf("Enter the size of the data bits (m): ");
+    scanf("%d",&data_bits_size);
+    int m[20];
+    accept(m,data_bits_size);
+    display(m,data_bits_size);
+    hamming_code(m,data_bits_size);
+    
+    printf("\n\n");
+    printf("Receiver's side\n");
     int n;
     printf("Enter code word size: ");
     scanf("%d",&n);
